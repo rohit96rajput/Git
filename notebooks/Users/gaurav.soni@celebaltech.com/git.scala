26ea983eef840aba1974f866792f328396562b8a
rohit96rajput <52233426+rohit96rajput@users.noreply.github.com>
2019-06-26 14:43:05 +0530

    fn

---
+++ notebooks/Users/gaurav.soni@celebaltech.com/git.scala
@@ -0,0 +1,23 @@
+// Databricks notebook source
+
+def sum(a:Int):Int=
+{
+  var fact=1
+  for(i <- 1 to a)
+  {
+    fact=fact*i
+  }
+  return fact
+}
+sum(5)
+
+// COMMAND ----------
+
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.expressions._
+import org.apache.spark.sql.types._
+import spark.implicits._
+import org.apache.spark.sql.Row
+
+// COMMAND ----------
+
